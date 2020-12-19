71f403d2ff048ee0e5fe9ae6c3aaf9b74f500d87
Bishoy Nader <bishoyngendy@gmail.com>
2020-12-19 18:43:05 +0200

    Add Latex and Cleaning up Code and UI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../core/auth.service';
 import { UserService } from '../core/user.service';
 import * as MediumEditor from 'medium-editor';
+import * as katex from 'katex';
 
 @Component({
   selector: 'home',
@@ -16,41 +17,67 @@ export class HomeComponent {
   editor: any;
   mathContent = '';
 
-  @ViewChild('media') media!: ElementRef;
-
-  ngAfterViewInit() {
-    const edit = this.media.nativeElement;
-    this.editor = new MediumEditor(edit, {
-      placeholder: false,
-    });
-    this.editor.subscribe('editableInput', (event: any) => {
-      if (event.inputType && event.inputType !== '') {
-        this.updateUser(this.editor.getContent());
-      }
-    });
-  }
-
-  updateUser(value: string) {
-    console.log('update', value);
-    this.userService.updateCurrentUser(value);
-    this.textValue = value;
-    console.log(this.mathContent);
-  }
+  @ViewChild('editorRef') editorRef!: ElementRef;
 
   constructor(
     public authService: AuthService,
     public userService: UserService,
-    private router: Router,
-    private fb: FormBuilder
+    private router: Router
   ) {
-    this.createForm();
     this.getUserData();
   }
 
-  createForm() {
-    this.textForm = this.fb.group({
-      textValue: [''],
+  ngAfterViewInit() {
+    const edit = this.editorRef.nativeElement;
+    this.editor = new MediumEditor(edit, {
+      placeholder: true,
     });
+    this.editor.subscribe('editableInput', (event: any) => {
+      if (event.data === '$') {
+        this.updateLatexNode();
+      }
+      this.updateUser(this.editor.getContent());
+    });
+  }
+
+  updateUser(value: string) {
+    this.userService.updateCurrentUser(value);
+    this.textValue = value;
+  }
+
+  updateLatexNode() {
+    const initialEditorContent = this.editor.getContent();
+    const firstIndex = initialEditorContent.indexOf('$');
+    if (firstIndex !== -1) {
+      const secondIndex = initialEditorContent
+        .substring(firstIndex + 1)
+        .indexOf('$');
+
+      if (secondIndex !== -1) {
+        const latextEquation = initialEditorContent.substring(
+          firstIndex,
+          firstIndex + secondIndex + 2
+        );
+        const latextEquationWithoutDollarSigns = latextEquation.substr(
+          1,
+          latextEquation.length - 2
+        );
+        const convertedLatex = katex.renderToString(
+          latextEquationWithoutDollarSigns,
+          {
+            throwOnError: true,
+          }
+        );
+        const tempNode = document.createElement('span');
+        tempNode.innerHTML = convertedLatex;
+        tempNode.childNodes[0].childNodes[0].remove();
+        const f = this.editor.exportSelection();
+        this.editor.setContent(
+          initialEditorContent.replace(latextEquation, tempNode.innerHTML)
+        );
+        document.getElementById('logout')?.focus();
+      }
+    }
   }
 
   getUserData() {
@@ -60,7 +87,7 @@ export class HomeComponent {
   }
 
   logout() {
-    this.authService.doLogout().then((res) => {
+    this.authService.logout().then(() => {
       this.router.navigate(['/']);
     });
   }
